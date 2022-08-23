214d2e8a464f4bddfb6d28699b224f52f0d76a57
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-08-23 17:29:33 +0100

    connected to mongo db with mongoose

---
+++ notFound/notfound.js
@@ -0,0 +1,5 @@
+const notFound = (req,res)=>res.status(404).send('page not Found')
+
+
+
+module.exports = notFound
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const task = require('./router/task')
+const notFound = require('./notFound/notfound')
+const connectDB = require('./connections/connectDB')
+require('dotenv').config()
 
 
 const PORT = 4040
@@ -9,6 +12,17 @@ app.use(express.static('./public'))
 
 app.use('/api/v1/tasks',task)
 
+app.use(notFound)
+
+const start = async ()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT,()=>console.log(`server is listening to port ${PORT}`))
+    } catch (error) {
+        
+    }
+}
+
+start()
 
 
-app.listen(PORT,()=>console.log(`server is listening to port ${PORT}`))
